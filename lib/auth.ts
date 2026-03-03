ba9fb7dc44c07138c8a266f932c23bc7a42c80c9
jtljrdn <jtlee@rmgj2.com>
2026-03-03 00:22:50 -0600

    supabase and better auth install

---
+++ lib/auth.ts
@@ -0,0 +1,11 @@
+import { betterAuth } from "better-auth";
+import { Pool } from "pg";
+
+const dialect = new Pool({
+  connectionString: "postgresql://postgres:password@localhost:5432/database",
+});
+
+export const auth = betterAuth({
+  database: { dialect, type: "postgresql" },
+  baseURL: "http://localhost:3000/",
+});
